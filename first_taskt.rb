74f8c673bddc1dcb2de2a8bb7f320ecbd5d73976
Dadushka <moiobk@gmail.com>
2024-05-13 13:36:57 +0300

    Second commit

--- first_taskt.rb
+++ first_taskt.rb
@@ -2,9 +2,10 @@ def first(arg)
   first_word = " компьютер"
   second_word = " компьютеров"
 
+  # число в строку
   value = arg.to_s
 
-  string = if value.end_with?("11")
+  string = if value.end_with?("11")  # заканчивается ли указанная строка заданным окончанием
              value + second_word
            elsif value.end_with?("1")
              value + first_word
